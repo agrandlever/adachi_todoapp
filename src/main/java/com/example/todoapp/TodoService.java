a3d1be683c3fcd1ad5c0e0d494b9758f629dcffa
agrandlever <agrandlever@gmail.com>
2026-08-25 15:26:21 +0900

    feat: Add logging to TodoService for create, update, and delete operations

--- src/main/java/com/example/todoapp/TodoService.java
+++ src/main/java/com/example/todoapp/TodoService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -24,13 +27,16 @@ public class TodoService {
 
     public void create(Todo todo) {
         todoMapper.insert(todo);
+        log.info("operation=登録 id={}", todo.getId());
     }
 
     public void update(Todo todo) {
         todoMapper.update(todo);
+        log.info("operation=編集 id={}", todo.getId());
     }
 
     public void delete(Long id) {
         todoMapper.deleteById(id);
+        log.info("operation=削除 id={}", id);
     }
 }
